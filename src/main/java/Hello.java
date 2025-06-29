d4261ff758451032646b9ba15e15f598a29b327a
karthik.patel06 <karthik.patel06@gmail.com>
2025-06-29 20:55:00 +0530

    Enhance Java lint workflow and update Hello class for lint testing

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,13 @@
-package src.main.java;
 public class Hello{  
     public static void main(String[] args) {
-        System.out.println("Hello from Java!");
+        if(true){
+        System.out.println("This should trigger lint errors");
+        }else{
+        System.out.println("This too");
+        }
+        int a = 0;
+        String sample = "";
+          System.out.println("This is a super-long message that will definitely exceed the maximum line length enforced by the style guide!");
     }
 
     public static String greet() {
