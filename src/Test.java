001d4da5b04471c59841bbd22e2d5e4b53c65d57
karthik.patel06 <karthik.patel06@gmail.com>
2025-06-29 17:11:37 +0530

    Add Maven configuration and update Java lint workflow
    - Introduced a new pom.xml file for Maven project management.
    - Added a step to check Maven installation in the Java lint workflow.
    - Removed the outdated google_checks.xml and Test.java files.
    - Updated the Test.java and HelloTest.java files to reflect new package structure and functionality.

--- src/Test.java
+++
@@ -1,6 +0,0 @@
-package src;
-class Test{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+package src.main.java;
+class Test{  
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+
+    public static String greet() {
+        return "Hello Test!";
+    }
+}
